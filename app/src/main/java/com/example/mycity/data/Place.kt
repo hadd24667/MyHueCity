caabac52297aaacff5c2f8f55af525a8c7f127d8
entropia <marta9kut@gmail.com>
2023-09-30 21:26:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mycity/data/Place.kt
@@ -0,0 +1,19 @@
+package com.example.mycity.data
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Place(
+    @StringRes val name: Int,
+    @StringRes val description: Int,
+    @DrawableRes val photo: Int,
+    @StringRes val address: Int,
+    val placeType: PlaceType
+)
+
+enum class PlaceType {
+    Restaurant,
+    Pubs,
+    Parks,
+    Other
+}
